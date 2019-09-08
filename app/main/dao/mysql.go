de78dff5cadc28c2491c2c9d9d0d957e92ea3f03
zzzzer <zzzzer91@gmail.com>
2019-09-08 19:07:28 +0800

    v0.0.8

--- app/main/dao/mysql.go
+++ app/main/dao/mysql.go
@@ -6,10 +6,8 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// DB 数据库链接单例
 var MysqlDB *gorm.DB
 
-// Database 在中间件中初始化mysql链接
 func init() {
 	d, err := db.InitMysql(conf.Params.MysqlUri)
 	if err != nil {
