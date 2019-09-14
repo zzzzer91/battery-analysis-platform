30f9f30642180b2d0bf6f02ca23cdce46d66fe65
zzzzer <zzzzer91@gmail.com>
2019-09-14 12:45:50 +0800

    v0.1.7 添加初步 test

---
+++ app/main/server/router_test.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"battery-anlysis-platform/app/main/middleware"
+	"battery-anlysis-platform/pkg/jd"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func getGinEngine() *gin.Engine {
+	gin.SetMode("release")
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(middleware.Session("test"))
+	register(r)
+	return r
+}
+
+func request(r http.Handler, method, path string, body io.Reader) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, body)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func jsonResponseEquel(t *testing.T, w *httptest.ResponseRecorder, code int, msg string, data interface{}) {
+	ast := assert.New(t)
+
+	var response map[string]interface{}
+	decoder := json.NewDecoder(w.Body)
+	decoder.UseNumber()
+	err := decoder.Decode(&response)
+	ast.Nil(err)
+
+	tmp, err := response["code"].(json.Number).Int64()
+	ast.Nil(err)
+
+	assert.Equal(t, int(tmp), code)
+	assert.Equal(t, response["msg"], msg)
+	assert.Equal(t, response["data"], data)
+}
+
+func TestGetLogin(t *testing.T) {
+	r := getGinEngine()
+	w := request(r, "GET", "/login", nil)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	jsonResponseEquel(t, w, jd.ERROR, "", nil)
+}
