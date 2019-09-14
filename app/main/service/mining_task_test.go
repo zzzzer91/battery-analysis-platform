77768523efb6c84349813c1944f4eadc74b32c33
zzzzer <zzzzer91@gmail.com>
2019-09-14 12:43:53 +0800

    v0.1.7 更新 test

--- app/main/service/mining_task_test.go
+++ app/main/service/mining_task_test.go
@@ -7,28 +7,16 @@ import (
 )
 
 func TestGetTaskList(t *testing.T) {
+	ast := assert.New(t)
 	ret, err := GetTaskList()
-	if err != nil {
-		t.Error(err)
-	}
+	ast.Nil(err)
 	s, _ := json.Marshal(ret)
 	t.Log(string(s))
 }
 
-//func TestGetTask(t *testing.T) {
-//	ret, err := GetTask("232eb995-e866-43ac-801b-4ea1bf35b5b6")
-//	if err != nil {
-//		t.Error(err)
-//	}
-//	s, _ := json.Marshal(ret)
-//	t.Log(string(s))
-//}
-
 func TestDeleteTask(t *testing.T) {
 	ast := assert.New(t)
 	count, err := DeleteTask("none")
-	if err != nil {
-		t.Error(err)
-	}
+	ast.Nil(err)
 	ast.Equal(int64(0), count)
 }
