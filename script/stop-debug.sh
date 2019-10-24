6f10c1197332fb95cb1f93f1a346265f39af73f0
zzzzer <zzzzer91@gmail.com>
2019-10-24 19:46:56 +0800

    v0.2.4 更新脚本文件

--- script/stop-debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ABSOLUTE_PROJECT_PATH="$(cd "$(dirname $0)/..";pwd)"
-cd $ABSOLUTE_PROJECT_PATH
-
-docker-compose -f docker-compose.debug.yml stop
--- script/stop-release.sh
+++ script/stop-release.sh
@@ -3,4 +3,4 @@
 ABSOLUTE_PROJECT_PATH="$(cd "$(dirname $0)/..";pwd)"
 cd $ABSOLUTE_PROJECT_PATH
 
-docker-compose -f docker-compose.release.yml stop
+docker-compose -f docker-compose.release.yml down
